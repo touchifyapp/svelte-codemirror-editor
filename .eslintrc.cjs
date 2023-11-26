184d2a64867affbb94c014ba7abeef064a84c761
Maxime LUCE <maxime@touchify.co>
2023-11-26 15:16:03 +0100

    fix: adjust eslint to new plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,23 @@
 module.exports = {
     root: true,
-    parser: "@typescript-eslint/parser",
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    plugins: ["svelte3", "@typescript-eslint"],
     ignorePatterns: ["*.cjs"],
-    overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-    settings: {
-        "svelte3/typescript": () => require("typescript"),
-    },
+    extends: ["eslint:recommended", "plugin:svelte/recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
     parserOptions: {
         sourceType: "module",
         ecmaVersion: 2020,
+        extraFileExtensions: [".svelte"],
     },
+    overrides: [
+        {
+            files: ["*.svelte"],
+            parser: "svelte-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+            },
+        },
+    ],
     env: {
         browser: true,
         es2017: true,
