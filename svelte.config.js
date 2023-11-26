fbb4a2a20146fb51bf8bdbc5246e12a680e2810d
Maxime LUCE <maxime@touchify.co>
2023-11-26 15:00:31 +0100

    fix: use new package structure
    fixed #25

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-    preprocess: preprocess(),
-
-    package: {
-        exports(filepath) {
-            return filepath.endsWith("package.json") || filepath.endsWith("index.ts");
-        },
-    },
+    preprocess: vitePreprocess(),
 
     kit: {
         adapter: adapter({
